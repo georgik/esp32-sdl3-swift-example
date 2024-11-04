078855e5d05a8d06992486030e255cc9c7ad8722
Juraj Michálek <juraj.michalek@espressif.com>
2024-11-04 12:06:15 +0100

    Upgrade to Swift 6.1 (#2)
    * use strings and remove C functions
    * add mount of filesystem in Swift

--- main/BridgingHeader.h
+++ main/BridgingHeader.h
@@ -8,11 +8,5 @@
 #include "SDL3_ttf/SDL_ttf.h"
 #include "pthread.h"
 #include "bsp/esp-bsp.h"
-#include "filesystem.h"
-
-const char* getBmpFilePath(void);
-const char* getDangerFilePath(void);
-float getRandomFloat(float min, float max);
-void logFloat(double value);
-const char* getFontFilePath(void);
-void getScoreText(int score, char* buffer, int bufferSize);
+#include "esp_vfs.h"
+#include "esp_littlefs.h"
