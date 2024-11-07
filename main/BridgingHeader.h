a14b1cf54134d30e1384a5c1672c3ca5349d8ad9
Juraj Michálek <juraj.michalek@espressif.com>
2024-11-07 12:37:30 +0100

    experiment: enable WiFi

--- main/BridgingHeader.h
+++ main/BridgingHeader.h
@@ -10,3 +10,17 @@
 #include "bsp/esp-bsp.h"
 #include "esp_vfs.h"
 #include "esp_littlefs.h"
+
+
+// ESP-IDF includes
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_sntp.h"
+#include "esp_http_client.h"
+#include "cJSON.h"
+
+#include "wrappers.h"
